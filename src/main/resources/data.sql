0ff6f3df9164e77e7251d3c4e5f43ea029c1b3a3
Roni Palacio <roninfo@gmail.com>
2018-12-28 08:03:42 -0200

    remove create table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
---create table exchange_value (id number, currency_from varchar(255),currency_to varchar(255), conversion_multiple, port number,0);
-
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple, port)
 values(10001,'USD','INR',65,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple, port)
